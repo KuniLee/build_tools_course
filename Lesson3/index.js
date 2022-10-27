5fe58d0cdd54ea502aafc7bfdd9b17e9517a0070
Yuriy Sitnikov <selnetys@gmail.ru>
2022-10-27 18:32:29 +0500

    Lesson3: stage 4

--- Lesson3/index.js
+++ Lesson3/index.js
@@ -19,6 +19,9 @@ const images = [
 const cardsContainer = document.querySelector('.row')
 for (const image of images) {
     const card = document.createElement('div')
-    card.insertAdjacentHTML('afterbegin', `<img src="${image.src}"></img><p>${image.title}</p>`)
+    card.className = 'col-12 col-sm-6 col-xl-4'
+    card.insertAdjacentHTML('afterbegin', `<div class="card"><img class="card-img-top" alt="${image.title}" 
+src="${image.src}">
+ <div class="card-body"><p class="card-title text-center">${image.title}</p></div></div>`)
     cardsContainer.append(card)
 }
